654a59d9505c8c6932abf93a528932041607f9b8
AlbertoCiuf <alberton.ciufici@gmail.com>
2021-11-02 17:04:30 +0100

    beta 2 griglie js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,29 @@ const grid = document.getElementById('grid');
 const playButton = document.getElementById('play-button');
 
 const difficulty = document.getElementById('difficulty');
-const chosenDifficulty = difficulty.options[difficulty.selectedIndex].value;
-const mainText = document.querySelector('main .main-text');
+let chosenDifficulty;
+
+// const mainText = document.querySelector('main .main-text');
+let square = document.querySelector('.square');
 
 playButton.addEventListener('click', function() {
-
-  mainText.classList.add('d-none');
-  grid.classList.add('active');
+  chosenDifficulty = parseInt(difficulty.value);
+  grid.innerHTML="";
+  console.log('click');
+  // mainText.classList.add('d-none');
+  // grid.classList.add('active');
 
   if (chosenDifficulty===1) {
     for (let i=0; i<100; i++) {
       generateSquare(grid);
+      square.innerHTML = i+1;
     }
+
   } else if (chosenDifficulty===2) {
     for (let i=0; i<81; i++) {
       generateSquare(grid);
     }
+
   } else {
     for (let i=0; i<49; i++) {
       generateSquare(grid);
@@ -31,16 +38,12 @@ function generateSquare(target) {
   const sq = document.createElement('div');
   sq.className = 'square';
 
-  // console.log(difficulty);
-  // console.log(chosenDifficulty);
-
   if (chosenDifficulty === 1) {
     sq.classList.add('easy');
-  } else if ( chosenDifficulty === 2) {
+  } else if (chosenDifficulty === 2) {
     sq.classList.add('normal');
   } else sq.classList.add('hard');
 
   target.append(sq);
-
-  
+  return sq;
 }
